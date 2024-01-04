643fe069bf85830a0c76c7d8948b7ca55bf632b2
sonnieson <mutiriaesther9@gmail.com>
2024-01-04 20:44:29 +0300

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-import marshal
-
-def print_names_from_pyc(pyc_file):
-    with open(pyc_file, 'rb') as file:
-         code = marshal.load(file)
-         names = [name for name in code.co_names if not name.startswith("__")]
-         names.sort()
-         for name in names:
-             print(name)
 
 if __name__ == "__main__":
-    pyc_file = "hidden_4.pyc"  # Update with the correct path if needed
-    print_names_from_pyc(pyc_file)
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
