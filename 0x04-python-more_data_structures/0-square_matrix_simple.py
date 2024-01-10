add1ddd29516b2c71f542dd89fdbf20f504ae8c3
sonnieson <mutiriaesther9@gmail.com>
2024-01-10 20:19:36 +0300

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+
+
 def square_matrix_simple(matrix=[]):
         """
         wordA function that computes the square
@@ -9,4 +11,4 @@ def square_matrix_simple(matrix=[]):
         for col in matrix:
             result = list(map(lambda x: x**2, col))
             new_matrix.append(result)
-       return new_matrix
+        return new_matrix
